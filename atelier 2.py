f8f3d01b6a513624801d4ba9309834760d69a566
hama374 <djaoudhamama15@gmail.com>
2026-03-09 21:26:51 -0400

    Créer la classe Voiture et ses attributs

---
+++ atelier 2.py
@@ -0,0 +1,8 @@
+class Voiture:
+
+    def _init_(self, matricule, annee, marque, kilometrage):
+        self.matricule = matricule
+        self.annee = annee
+        self.marque = marque
+        self.kilometrage = kilometrage
+        self.chauffeur = None
