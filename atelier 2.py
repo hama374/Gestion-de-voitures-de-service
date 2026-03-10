8db6dbb0d4c3a6c0390ee1d468dff4c97218d7af
hama374 <djaoudhamama15@gmail.com>
2026-03-09 22:20:02 -0400

    Fix indentation in constructor of Voiture class

--- atelier 2.py
+++ atelier 2.py
@@ -1,6 +1,6 @@
 class Voiture:
 
-    def __init__(self, matricule, annee, marque, kilometrage):
+    def  __init__(self, matricule, annee, marque, kilometrage):
         self.matricule = matricule
         self.annee = annee
         self.marque = marque
